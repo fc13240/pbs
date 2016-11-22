9bc44bfa70256b73a9d9469850fe9c37519b46a8
qz <306203478@qq.com>
2016-11-22 14:39:08 +0800

    创建BrandCategory

---
+++ src/main/java/zhuanli/domain/BrandCategory.java
@@ -0,0 +1,20 @@
+package zhuanli.domain;
+
+public class BrandCategory {
+	private int category_id;
+	private String categoryName;
+	public int getCategory_id() {
+		return category_id;
+	}
+	public void setCategory_id(int category_id) {
+		this.category_id = category_id;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	
+	
+}
