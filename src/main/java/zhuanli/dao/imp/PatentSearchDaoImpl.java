443fd3ddda0f28f22171d9a459d4c879f13ba3c5
CaiWei <15256085730@sina.cn>
2016-12-20 09:41:29 +0800

    pbs格式化

--- src/main/java/zhuanli/dao/imp/PatentSearchDaoImpl.java
+++ src/main/java/zhuanli/dao/imp/PatentSearchDaoImpl.java
@@ -1,4 +1,5 @@
 package zhuanli.dao.imp;
+
 import static com.mongodb.client.model.Filters.eq;
 
 import java.util.ArrayList;
@@ -13,34 +14,34 @@ import com.mongodb.client.MongoDatabase;
 import zhuanli.dao.PatentSearchDao;
 import zhuanli.domain.MongoPatent;
 
-public class PatentSearchDaoImpl implements PatentSearchDao{
-//	private static final int DEFAULT_SLOP = 5;
-//	private static final int DEFAULT_EXACT_MATCH_MIN_LENGTH = 3;
+public class PatentSearchDaoImpl implements PatentSearchDao {
+	// private static final int DEFAULT_SLOP = 5;
+	// private static final int DEFAULT_EXACT_MATCH_MIN_LENGTH = 3;
 	private MongoClient mongoClient;
 
 	public PatentSearchDaoImpl(MongoClient mongoClient) {
 		this.mongoClient = mongoClient;
 	}
-	
+
 	@Override
-	public  MongoPatent findMongoPatentByAppNo(String appNo){	
+	public MongoPatent findMongoPatentByAppNo(String appNo) {
 		MongoDatabase db = mongoClient.getDatabase("sopatent");
 		MongoCollection<Document> collection = db.getCollection("patent");
-		Document doc= collection.find(eq("appNo",appNo)).first();
-		if(doc != null){
-		return convertDocToMongoPatent(doc);
-		}else{
+		Document doc = collection.find(eq("appNo", appNo)).first();
+		if (doc != null) {
+			return convertDocToMongoPatent(doc);
+		} else {
 			return null;
 		}
 	}
-	
+
 	private MongoPatent convertDocToMongoPatent(Document doc) {
 		MongoPatent MongoPatent = new MongoPatent();
 		MongoPatent.setId(doc.getObjectId("_id"));
 		MongoPatent.setPatentAbstract(doc.getString("abstract"));
 		return MongoPatent;
 	}
-	
+
 	@SuppressWarnings({ "unchecked", "unused" })
 	private String array2Str(Document doc, String fieldName) {
 		ArrayList<String> values = doc.get(fieldName, ArrayList.class);
@@ -49,7 +50,7 @@ public class PatentSearchDaoImpl implements PatentSearchDao{
 		}
 		return null;
 	}
-	
+
 	@SuppressWarnings("unused")
 	private String getSubField(Document doc, String parentField, String subField) {
 		Document subDoc = doc.get(parentField, Document.class);
@@ -59,7 +60,4 @@ public class PatentSearchDaoImpl implements PatentSearchDao{
 		return subDoc.getString(subField);
 	}
 
-	
-}	
-
-  
+}
